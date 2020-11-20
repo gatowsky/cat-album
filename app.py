4ff806cc3f30f5089b8796e248c0d73c21157950
Cristhian Roa <cristhian@designisdead.com>
2020-11-20 18:30:19 +0100

    Added main to run app.py when called

--- app.py
+++ app.py
@@ -5,3 +5,6 @@ app = Flask(__name__)
 def test_deployment():
     print("I am running")
     return "app succesfully deployed to Kubernetes"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
