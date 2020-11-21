8e09e146fb57495df73ce028ddd6289029365d71
William Arias <warias@gitlab.com>
2020-11-21 13:28:11 +0000

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 @app.route('/')
 def test_deployment():
     print("I am running")
-    return "app succesfully deployed to Kubernetes"
+    return "Hello, 世界"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
