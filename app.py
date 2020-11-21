e411693cc9329ecba8afcab4fc0c50feb6d7cc50
William Arias <warias@gitlab.com>
2020-11-21 17:38:08 +0000

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ def test_deployment():
     print("I am running")
     return "Hello, 世界,  app deployed on Kubernetes- welcome cloud native !!"
 
+@app.route('/helm')
+def helm_hi():
+    return "Hello Helm wrapper"
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
